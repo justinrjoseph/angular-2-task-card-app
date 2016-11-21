6b6583f2b64fbb9eb52f70d6a03c7b158f7345f7
Justin Joseph <justin.joseph0@yahoo.com>
2016-11-21 12:44:34 -0500

    Use input property binding to display task title

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,15 @@
  import { Component } from '@angular/core';
 
+ import { Task } from './models/task';
+
  @Component({
 		moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html'
  })
- export class AppComponent {}
+ export class AppComponent {
+   task1: Task = {
+     content: 'Learn to code',
+     completed: false
+   };
+ }
